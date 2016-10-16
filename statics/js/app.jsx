2be78a2ae9f3c7592a137ceef7abf74bdb58daf0
Yan Hong <onlyhongyan@gmail.com>
2016-10-15 17:58:31 -0700

    apply style to current react app

--- statics/js/app.jsx
+++ statics/js/app.jsx
@@ -13,7 +13,7 @@ var Student = React.createClass({
     },
     render: function() {
         return (<div>
-            <h2 onClick={this.handleStudentClick}>{this.props.data.firstName}</h2>
+            <h1 onClick={this.handleStudentClick}>{this.props.data.firstName}</h1>
         </div>)
     }
 });
@@ -21,17 +21,63 @@ var Student = React.createClass({
 var StudentDetail = React.createClass({
 
     render: function() {
-        return(<div>
-            <h2>First Name: {this.props.data.firstName}</h2>
-            <h2>Last Name: {this.props.data.lastName}</h2>
-            <h2>Email: {this.props.data.email}</h2>
-            <h2>Age: {this.props.data.age}</h2>
+        return(<div className="student-detail">
+            <span>First Name</span>
+            <p>{this.props.data.firstName}</p>
+            <span>Last Name</span>
+            <p>{this.props.data.lastName}</p>
+            <span>Age</span>
+            <p>{this.props.data.email}</p>
+            <span>Email</span>
+            <p>{this.props.data.age}</p>
         </div>)
     }
 
 });
 
+var StudentForm = React.createClass({
+    submitForm: function() {
+        var newStudent = {
+            firstName: document.getElementById('firstName').value,
+            lastName: document.getElementById('lastName').value,
+            age: document.getElementById('age').value,
+            email: document.getElementById('email').value
+        };
+        var ReactThis = this;
+        axios.post(host + 'student/new', newStudent)
+            .then(function(response) {
+                ReactThis.props.insert(response.data);
+
+            })
+            .catch(function(err) {
+                console.log(err);
+            })
+
+    },
+    render: function() {
+        return (
+            <div>
+                <input type="text" placeholder="first name" id="firstName"/>
+                <input type="text" placeholder="last name" id="lastName"/>
+                <input type="text" placeholder="age" id="age"/>
+                <input type="text" placeholder="email" id="email"/>
+                <button onClick={this.submitForm}>Submit</button>
+            </div>
+        )
+    }
+});
+
 var StudentList = React.createClass({
+
+    insertStudent: function(data) {
+        var tempStudent = this.state.studentList;
+        tempStudent.push(data);
+        this.setState({
+            studentList: tempStudent,
+            showForm: false
+        });
+
+    },
 
     updateDetail: function(data) {
         this.setState({
@@ -39,15 +85,16 @@ var StudentList = React.createClass({
         });
     },
 
-   getInitialState: function() {
-       return {
-           studentList: [],
-           studentDetail: {}
-       }
-   },
+    getInitialState: function() {
+        return {
+            studentList: [],
+            studentDetail: {},
+            showForm: false,
+        }
+    },
     componentWillMount: function() {
 
-      var ReactThis = this;
+        var ReactThis = this;
 
         axios.get(host + 'students')
             .then(function(response) {
@@ -56,29 +103,36 @@ var StudentList = React.createClass({
                 });
             })
             .catch(function(err) {
-               console.log(err);
+                console.log(err);
             });
+    },
+    showAddNewForm: function() {
+        this.setState({
+            showForm: true
+        });
     },
     render: function() {
         var ReactThis = this;
         if (this.state.studentDetail.firstName) {
-            var sdNode = (
-                <div>
-                    <h1>Student Detail:</h1>
-                    <StudentDetail data={this.state.studentDetail}/>
-                </div>
-            )
+            var sdNode = <StudentDetail data={this.state.studentDetail}/>
         } else {
             var sdNode = "";
         }
-       return (
-           <div>
-           <h1>Student List:</h1>
-        {this.state.studentList.map(function(student) {
-            return <Student key={student._id} data={student} update={ReactThis.updateDetail} />;
-       })}
-               {sdNode}
-           </div>
+        return (
+            <div>
+                <div className="student-list">
+                    {this.state.studentList.map(function(student) {
+                        return <Student key={student._id} data={student} update={ReactThis.updateDetail} />;
+                    })}
+
+                    <div onClick={this.showAddNewForm}>
+                        <h1 className="add-new">Add New</h1>
+                    </div>
+                </div>
+                {sdNode}
+
+                { this.state.showForm ? <StudentForm insert={this.insertStudent}/> : ""}
+            </div>
         );
     }
 });
